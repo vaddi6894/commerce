0a456b1b2208a46c1768f3318cb1308c63df19b6
Vaddi Sri Manikarnik <manikarnikvaddi8@gmail.com>
2025-08-16 00:41:37 +0530

    changes api.js

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,7 +1,9 @@
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.REACT_APP_API_URL || "https://commerce-shoppie.onrender.com",
+  baseURL:
+    process.env.REACT_APP_API_URL ||
+    "https://commerce-shoppie.onrender.com/api",
   withCredentials: true,
 });
 
